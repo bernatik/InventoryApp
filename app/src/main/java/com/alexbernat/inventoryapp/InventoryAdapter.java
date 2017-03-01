43f23537e5f9a620e797bfd8612f583d2661bfcf
Alexander <bernatik@tut.by>
2017-03-01 20:00:54 +0300

    realized product sales from CatalogActivity

--- app/src/main/java/com/alexbernat/inventoryapp/InventoryAdapter.java
+++ app/src/main/java/com/alexbernat/inventoryapp/InventoryAdapter.java
@@ -1,7 +1,10 @@
 package com.alexbernat.inventoryapp;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +21,8 @@ import com.alexbernat.inventoryapp.data.InventoryContract;
 
 public class InventoryAdapter extends CursorAdapter {
 
+    public static final int SALE_INCREMENT = 1;
+
     public InventoryAdapter(Context context, Cursor cursor) {
         super(context, cursor, 0);
     }
@@ -28,7 +33,7 @@ public class InventoryAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, final Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
         String productName = cursor.getString(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_NAME_PRODUCT_NAME));
         int quantity = cursor.getInt(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_NAME_QUANTITY));
         double price = cursor.getDouble(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_NAME_PRICE));
@@ -40,10 +45,23 @@ public class InventoryAdapter extends CursorAdapter {
         tvPrice.setText(String.valueOf(price));
 
         Button btnSale = (Button) view.findViewById(R.id.sale_button);
+        btnSale.setTag(cursor.getPosition());
         btnSale.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "I clicked Sale button", Toast.LENGTH_SHORT).show();
+                cursor.moveToPosition((int) v.getTag());
+                String productName = cursor.getString(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_NAME_PRODUCT_NAME));
+                long id = cursor.getLong(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_NAME_ID));
+                int quantity = cursor.getInt(cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_NAME_QUANTITY));
+                quantity = quantity - 1;
+                ContentValues values = new ContentValues();
+                values.put(InventoryContract.InventoryEntry.COLUMN_NAME_QUANTITY, quantity);
+                Uri currentUri = ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI, id);
+                int rowsUpdated = context.getContentResolver().update(currentUri, values, null, null);
+                if (rowsUpdated != 0) {
+                    String message = context.getString(R.string.toast_sale_successful, SALE_INCREMENT, productName);
+                    Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
